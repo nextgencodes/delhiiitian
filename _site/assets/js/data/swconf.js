e60867fb9a5b497ae50aad633316ea6ce2c8c6d4
nextgencodes <ashokatmindtree@gmail.com>
2025-02-27 02:35:06 +0530

    ruby update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740603619',resources: [
+    cacheName: 'chirpy-1740603898',resources: [
       '/assets/css/jekyll-theme-darkgen.css',
       '/',
       
