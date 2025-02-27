c68c25be1f7837ad7d417f8059c4f339487f8d81
nextgencodes <ashokatmindtree@gmail.com>
2025-02-27 23:24:13 +0530

    added adsense code

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740663507',resources: [
+    cacheName: 'chirpy-1740678820',resources: [
       '/assets/css/jekyll-theme-darkgen.css',
       '/',
       
