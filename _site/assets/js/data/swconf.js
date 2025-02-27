41bbd71657e2bce48439c5fff01ce6173d1d0d1c
nextgencodes <ashokatmindtree@gmail.com>
2025-02-27 23:38:35 +0530

    download button test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740678820',resources: [
+    cacheName: 'chirpy-1740679709',resources: [
       '/assets/css/jekyll-theme-darkgen.css',
       '/',
       
